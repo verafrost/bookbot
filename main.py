7c5f8c8a982fd5c656cccff2569a710fe78228e8
verafrost <verajohannafrost@outlook.com>
2024-03-18 12:21:57 +0000

    define main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main() -> None:
+    folder = "books"
+    book = "frankenstein.txt"
+    pathfile = f"{folder}/{book}"
+
+    with open(pathfile) as f:
+        file_contents = f.read()
+
+if __name__ == '__main__':
+    main()
