1fcd54b1cd415747a3455ff5ac5e8161477e1672
verafrost <verajohannafrost@outlook.com>
2024-03-18 12:48:51 +0000

    add wordcount print function

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ def main() -> None:
     with open(pathfile) as f:
         file_contents = f.read()
 
+    words = file_contents.split()
+    word_count = len(words)
+
+    print(word_count)
+
 if __name__ == '__main__':
     main()
